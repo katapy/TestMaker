ec73716a5bd8e101eed44263c8182186683f3906
katapy <katapy.tennis@gmail.com>
2022-07-15 20:47:51 +0900

    Python class become camel case.

--- app/database/event/table_header_event.py
+++ app/database/event/table_header_event.py
@@ -2,13 +2,13 @@
 """
 
 import string
-from database.model.table_header import table_header
+from database.model.table_header import TableHeader
 from main import database
 
 def get_disply_name(table_name: string, column_name: string):
     """Get the display name of the table header from RDB.
     """
-    return table_header.query\
-        .filter(table_header.table_name==table_name,\
-            table_header.column_name==column_name)\
+    return TableHeader.query\
+        .filter(TableHeader.table_name==table_name,\
+            TableHeader.column_name==column_name)\
         .first().display_name
--- app/routes/login.py
+++ app/routes/login.py
@@ -3,14 +3,14 @@
 from flask import render_template, redirect, request, abort, flash, url_for
 from flask_login import login_user
 import routes
-from main import app_user
+from main import AppUser
 
 @routes.bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         name = request.form.get('name')
         password = request.form.get('password')
-        user = app_user.query.filter_by(name=name).first()
+        user = AppUser.query.filter_by(name=name).first()
         # if check_password_hash(user.password, password):
         if user is not None and user.password == password:
             login_user(user)
